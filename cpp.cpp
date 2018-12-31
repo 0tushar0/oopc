71d5abfac8548897faabc2b54a0ce48e53bc16ba
0tushar0 <0tushar0@gmail.com>
2018-12-30 23:39:18 -0600

    first commit

---
+++ cpp.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+void fun(){
+	printf("Hello world\n");
+}
+
+
+void fun_ptr(){
+	printf("fun_ptr function\n");
+}
+
+int main(){
+
+	fun_ptr();	// prints fun_ptr function
+	
+	// can block scope at will in C++ as well
+	{
+		void (*fun_ptr)(void) = &fun;
+		// works
+		// (fun_ptr)();
+
+		// doesn't print - compiles
+		// fun_ptr;
+
+		// prints
+		// printf("addr: %x\n", &fun_ptr);
+		fun_ptr();	// prints Hello world - reason is C is block scoped
+	}
+
+	fun_ptr();	// prints fun_ptr function
+			// if not block scope, would print Hello world
+	// printf("addr: %x\n", &fun_ptr);
+	return 0;
+}
---
+++ test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void fun(int a){
+	printf("The value of a is %d\n", a);
+}
+
+
+int main(){
+	void (*fun_ptr)(int) = &fun;
+
+	/*  The above line is two statements
+	 *  void (*fun_ptr)(int);
+	 *  fun_ptr = &fun;
+	*/
+	
+	(*fun_ptr)(10);
+	return 0;
+}
+
+
