d4be594e1e2596245d0e5496b51e9ea680d49d7f
0tushar0 <0tushar0@gmail.com>
2019-01-03 23:02:25 -0600

    focusing on oop file, rather than experimentation

--- Makefile
+++ Makefile
@@ -1,24 +1,10 @@
 # files contained right now: 
 
-# cpp.cpp 
 # main.c
-# stdc89.c
-# test.c
 
-# default make, since most strict compiling conditions
-standard : stdc89.c
-	gcc -std=c89 -o standardC stdc89.c
-
-oop : cpp.cpp
-	g++ -c cpp.cpp
-	g++ -o oop cpp.o
-
+# default make
 c : main.c
 	gcc -o C main.c
 
-test : test.c
-	gcc -o original test.c
-
-clean :
-	# -f because not all files may be generated
-	rm -f *.o standardC oop C original
+clean : 
+	rm C
--- main.c
+++ main.c
@@ -15,22 +15,11 @@ int main(){
 
 	fun_ptr();	// prints fun_ptr function
 	
-	// can block scope at will in C
 	{
 		void (*fun_ptr)(void) = &fun;
-		// works
-		// (fun_ptr)();
-
-		// doesn't print - compiles
-		// fun_ptr;
-
-		// prints
-		// printf("addr: %x\n", &fun_ptr);
 		fun_ptr();	// prints Hello world - reason is C is block scoped
 	}
 
-	fun_ptr();	// prints fun_ptr function
-			// if not block scope, would print Hello world
-	// printf("addr: %x\n", &fun_ptr);
+	fun_ptr();
 	return 0;
 }
